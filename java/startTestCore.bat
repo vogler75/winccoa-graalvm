c5b414d38fbac49c96cc4a26bc73d6b802585652
Andreas Vogler <andreas.vogler@me.com>
2024-05-23 20:03:54 +0200

    Renamed directory to winccoa-graalvm

--- java/startTestCore.bat
+++ java/startTestCore.bat
@@ -1,1 +1,1 @@
-node.exe --vm.cp="build\classes\java\main" "C:\Siemens\Automation\WinCC_OA\3.20\data/nodejs/winccoa-manager/lib/bootstrap.js" -PROJ "Test320" -pmonIndex 12 winccoa-graalvm-nodejs/nodejs/testCore.js
+node.exe --vm.cp="build\classes\java\main" "C:\Siemens\Automation\WinCC_OA\3.20\data/nodejs/winccoa-manager/lib/bootstrap.js" -PROJ "Test320" -pmonIndex 12 winccoa-graalvm/nodejs/testCore.js
